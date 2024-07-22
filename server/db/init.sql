aaec7d2ecc5e4cb4714fb5f3731b0ce568a4a2dd
akshayashirvad <merigalaakshayashirvad2003@gamil.com>
2024-07-22 17:53:55 +0530

    Secrets added

--- server/db/init.sql
+++ server/db/init.sql
@@ -1,53 +1,3 @@
-
-/*CREATE DATABASE yelp;
-
-
-CREATE USER postgres WITH PASSWORD 'password123';
-
-
-GRANT ALL PRIVILEGES ON DATABASE yelp TO postgres;
-
-
-\c yelp
-
-
-
-CREATE TABLE IF NOT EXISTS restaurants (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    address TEXT,
-    rating NUMERIC,
-    location VARCHAR(255),
-    price_range VARCHAR(50)
-);
-
-
-CREATE TABLE IF NOT EXISTS reviews (
-    id BIGSERIAL PRIMARY KEY,
-    restaurant_id BIGINT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    review TEXT NOT NULL,
-    rating INTEGER NOT NULL CHECK (rating >= 1 AND rating <= 5),
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(id) ON DELETE CASCADE
-);
-
-
-
-
-
-
-select *
-from restaurants
-    left join(
-        select restaurant_id,
-            count(*),
-            TRUNC(AVG(rating, 1)) as average_rating
-        from reviews
-        group by restaurant_id
-    ) reviews on restaurants.id = reviews.restaurant_id;*/
-
- -- Check if the database exists before creating it
 DO $$
 BEGIN
    IF NOT EXISTS (
@@ -105,4 +55,3 @@ LEFT JOIN (
     FROM reviews
     GROUP BY restaurant_id
 ) reviews ON restaurants.id = reviews.restaurant_id;
-   
