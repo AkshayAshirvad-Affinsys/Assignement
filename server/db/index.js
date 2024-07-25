f30b04f031f0deff81a55bf0fd83f70ec236b6cb
akshayashirvad <merigalaakshayashirvad2003@gamil.com>
2024-07-25 09:57:34 +0530

    Security added basic

--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,21 @@
-/*const { Pool } = require("pg");
+const fs = require('fs');
+const { Pool } = require('pg');
 
-const pool = new Pool();
+// Read secrets from files
+const pgUser = fs.readFileSync('/run/secrets/user', 'utf8').trim();
+const pgPassword = fs.readFileSync('/run/secrets/password', 'utf8').trim();
+const pgDatabase = fs.readFileSync('/run/secrets/database', 'utf8').trim();
+
+const pool = new Pool({
+  user: pgUser,
+  host: process.env.PGHOST || 'localhost',
+  database: pgDatabase,
+  password: pgPassword,
+  port: process.env.PGPORT || 5432,
+});
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
-};*/
+};
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'password123',
-  database: process.env.DB_NAME || 'yelp'
-});
 
